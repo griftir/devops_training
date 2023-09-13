08335eea7db725f042b0363eee517b189860b0fb
Vince <griftir@users.noreply.github.com>
2023-09-13 10:54:23 -0400

    raedme

---
+++ 1-Containers/README.md
@@ -0,0 +1,18 @@
+# Containers
+
+## Commands
+
+### Running the toy app
+```
+go run main.go
+```
+
+### Testing the toy app
+```
+go test ./...
+```
+
+### Building the toy app
+```
+go build -o hello ./main.go
+```
--- 1-Containers/main.go
+++ 1-Containers/main.go
@@ -12,6 +12,6 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", helloHandler)
-
+	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", server)
 }
